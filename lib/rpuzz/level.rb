4298eecdf4f57434ed366967b35d418c6c9356b0
Gavin Heavyside <gavin@heavyside.co.uk>
2009-08-01 22:03:48 +0100

    functions to load & manipulate game board |

--- lib/rpuzz/level.rb
+++ lib/rpuzz/level.rb
@@ -1,6 +1,77 @@
 module RPuzz
   class Level
+    attr_reader :level
+    attr_reader :author
+    attr_reader :name
+
     def initialize(level)
+      load_level(level)
+    end
+
+    def paint(io)
+      @level.each {|line| io.puts line}
+    end
+    
+    def at(x,y)
+      @level[x][y,1]
+    end
+
+    def above(x,y)
+      at(x-1,y)
+    end
+    
+    def below(x,y)
+      at(x+1,y)
+    end
+    
+    def left_of(x,y)
+      at(x,y-1)
+    end
+    
+    def right_of(x,y)
+      at(x,y+1)
+    end
+    
+    def move_up(x,y)
+      swap(x-1,y,x,y) if above(x,y) == " "
+    end
+    
+    def move_down(x,y)
+      swap(x+1,y,x,y) if below(x,y) == " "
+    end
+    
+    def move_right(x,y)
+      swap(x,y+1,x,y) if right_of(x,y) == " "
+    end
+    
+    def move_left(x,y)
+      swap(x,y,x,y-1) if left_of(x,y) == " "
+    end
+    
+    def eat_diamond(x,y)
+      set(x,y, " ") if at(x,y) == "*"
+    end
+    
+  private
+  
+    def set(x,y, character)
+      @level[x][y] = character
+    end
+    
+    def swap(x,y,x2,y2)
+      tmp = at(x,y)
+      set(x,y, at(x2,y2))
+      set(x2,y2,tmp)
+    end
+    
+    def load_level(level)
+      @level = []
+      File.open(level).each_line do |line|
+        line = line.chomp
+        @level.push(line) if line =~ /#/
+        @author = $1 if line =~ /^AUTHOR (.*)$/
+        @name = $1 if line =~ /^LEVELNAME (.*)$/
+      end
     end
   end
 end
--- spec/rpuzz/level_spec.rb
+++ spec/rpuzz/level_spec.rb
@@ -2,10 +2,80 @@ require File.join(File.dirname(__FILE__), '/../spec_helper')
 
 module RPuzz
   describe Level do
-    
-    it "should load a level description from file" do
-      RPuzz::Level.new('level/octothorpe.level')
+    before do
+      @level = RPuzz::Level.new(File.join(File.dirname(__FILE__), '/../../test/octothorpe.rpuzz'))
     end
     
+    it "should load a level from a file" do
+      @level.level.size.should == 20
+      @level.name.should == "octothorpe"
+      @level.author.should == "Jim Thomson"
+      @level.level.each {|line| line.size.should == 19}
+    end
+    
+    it "should print the level to the specified IO thingy" do
+      str = StringIO.new
+      @level.paint(str)
+      bits = str.string.split("\n")
+      bits.size.should == 20
+      bits.each{|line| line.size.should == 19}
+      bits[1][1,1].should == "O"
+      bits[2][0,1].should == "#"
+      bits[4][5,1].should == "*"
+      bits[4][6,1].should == "+"
+    end
+
+    it "should return the character at a location" do
+      @level.at(1,1).should == "O"
+      @level.at(2,0).should == "#"
+      @level.at(4,5).should == "*"
+      @level.at(4,6).should == "+"
+    end
+    
+    it "should return the characters surrounding a location" do
+      @level.above(4,6).should == "#"
+      @level.below(4,6).should == "#"
+      @level.left_of(4,6).should == "*"
+      @level.right_of(4,6).should == " "
+    end
+    
+    it "should move a character when requested and there is nothing blocking it" do
+      @level.at(4,6).should == "+"
+      @level.move_right(4,6).should_not be_nil
+      @level.at(4,6).should == " "
+      @level.at(4,7).should == "+"
+      @level.move_up(4,7).should_not be_nil
+      @level.at(4,7).should == " "
+      @level.at(3,7).should == "+"
+      @level.move_down(3,7).should_not be_nil
+      @level.at(3,7).should == " "
+      @level.at(4,7).should == "+"
+      @level.move_left(4,7).should_not be_nil
+      @level.at(4,7).should == " "
+      @level.at(4,6).should == "+"
+    end
+
+    it "should not move a character when requested but something is blocking it" do
+      @level.at(4,6).should == "+"
+      @level.move_up(4,6).should be_nil
+      @level.at(4,6).should == "+"
+      @level.at(3,6).should == "#"
+      @level.move_down(4,6).should be_nil
+      @level.at(4,6).should == "+"
+      @level.at(5,6).should == "#"
+    end
+    
+    it "should remove a diamond when requested" do
+      @level.at(4,5).should == "*"
+      @level.eat_diamond(4,5).should_not be_nil
+      @level.at(4,5).should == " "
+    end
+  
+    it "should not remove anything if it is not a diamond" do
+      @level.at(3,5).should == "O"
+      @level.eat_diamond(3,5).should be_nil
+      @level.at(3,5).should == "O"
+    end
+  
   end
 end
